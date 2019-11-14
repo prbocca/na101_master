ef8eb8d6dee054294300c06af9378fa538808a0b
Pablo Rodriguez-Bocca <prodriguezbocca@gmail.com>
2019-11-14 15:45:54 -0300

    na update

--- homeworks_common.r
+++ homeworks_common.r
@@ -1,6 +1,8 @@
 
-
-# Funciones auxiliares
+#########################################################
+##
+## Funciones auxiliares
+##
 #########################################################
 
 qpaste = function(...) paste(..., sep="")
@@ -50,6 +52,136 @@ saveAsGEXF = function(g, filepath="converted_graph.gexf")
   print(output, filepath, replace=T)
 }
 
+
+
+#########################################################
+##
+## Binary classification
+##
+#########################################################
+
+
+last = function(x, ...){
+  tail(x, n=1, ...)
+}
+
+linelight <- function(x,y, lty='dashed', col='lightgray', ...) {
+  # highlight a point with lines running to the axes.
+  left = par('usr')[1]
+  bot = par('usr')[3]
+  segments(left,y, x,y, lty=lty, col=col, ...)
+  segments(x,bot,  x,y, lty=lty, col=col, ...)
+}
+
+
+#https://brenocon.com/blog/2009/04/binary-classification-evaluation-in-r-via-rocr/
+binary_eval <- function(pred,labels, cutoff='naive', repar=TRUE, ...) {
+  # Various binary classification evaluation plots and metrics
+  library(ROCR)
+  # plot(performance(prediction(pred,y),'acc'))
+  rocr_pred = prediction(pred,labels)
+  acc = performance(rocr_pred,'acc')
+  f1 = performance(rocr_pred,'f')
+  auc = performance(rocr_pred,'auc')@y.values[[1]]
+  roc = performance(rocr_pred,'rec','spec')
+  bac = if (rocr_pred@n.pos[[1]] != rocr_pred@n.neg[[1]])
+    sapply(1:length(roc@x.values[[1]]), function(i)
+      mean(c(roc@x.values[[1]][i], roc@y.values[[1]][i])))
+  else
+    rep(-1,length(pred))
+  # sensspec = performance(rocr_pred,'rec','spec')
+  pr_curve = performance(rocr_pred,'prec','rec')
+  rp_curve = performance(rocr_pred,'rec','prec')
+  
+  printf("AUC = %.3f\n", auc)
+  
+  if (cutoff=='naive') {
+    if (all(pred>=0) & all(pred<=1)) {
+      printf("Predictions seem to be probabilities, so ")
+      cutoff = 0.5
+    } else if (any(pred<0) & any(pred>0)) {
+      printf("Predictions seem to be real-valued scores, so ")
+      cutoff = 0
+    } else {
+      warning("cant tell what naive cutoff should be")
+      cutoff = NULL
+    }
+    printf("using naive cutoff %s:\n", cutoff)
+  } else if (class(cutoff)=='character') {
+    printf("Using %s-best cutoff ", cutoff)
+    if (cutoff=='bac') {
+      perf = NULL
+      perf_y = bac
+    } else {
+      perf = performance(rocr_pred, cutoff, ...)
+      perf_y = perf@y.values[[1]]
+    }
+    cutoff_ind = which.max(perf_y)
+    cutoff = if (cutoff=='prbe') perf@x.values[[1]][1] else rocr_pred@cutoffs[[1]][cutoff_ind]
+    printf("%f\n", cutoff)
+  } else {
+    printf("For cutoff %s:\n", cutoff)
+  }
+  cutoff_ind = last(which(rocr_pred@cutoffs[[1]] >= cutoff))
+  
+  if (repar) par(mfrow=c(2,2))
+  
+  pp = function(perf)  {
+    if (length(cutoff_ind)>0 && is.finite(cutoff_ind)) {
+      x=perf@x.values[[1]][cutoff_ind]
+      y=perf@y.values[[1]][cutoff_ind]
+      points(x,y, col='blue')
+      linelight(x,y, col='lightblue')
+    }
+  }
+  plot(acc); pp(acc)
+  plot(f1); pp(f1)
+  plot(roc); pp(roc)
+  abline(a=1,b=-1,lty='dashed',col='gray')
+  legend('bottomleft',legend=sprintf("AUC = %.3f",auc))
+  plot(rp_curve); pp(rp_curve)
+  pp = function(ind,...) points(rp_curve@x.values[[1]][ind], rp_curve@y.values[[1]][ind], ...)
+  best_f1 = which.max(f1@y.values[[1]])
+  pp(best_f1, pch=2,col='green')
+  f05 = performance(rocr_pred,'f',beta=0.5)
+  best_f05 = which.max(f05@y.values[[1]])
+  pp(best_f05,pch=2,col='green')
+  f2 = performance(rocr_pred,'f',beta=2)
+  best_f2 = which.max(f2@y.values[[1]])
+  pp(best_f2,pch=2,col='green')
+  
+  prbe = performance(rocr_pred,'prbe')@y.values[[1]]
+  linelight(prbe,prbe,col='lightgray')
+  
+  # printf("Acc = %.3f\n", mean((pred >= cutoff) == (labels > 0)))
+  printf("Acc %.3f, ", acc@y.values[[1]][cutoff_ind])
+  
+  printf("  F %.3f, Prec %.3f, Rec %.3f, Spec %.3f",
+         f1@y.values[[1]][cutoff_ind],
+         pr_curve@y.values[[1]][cutoff_ind],
+         pr_curve@x.values[[1]][cutoff_ind],
+         roc@x.values[[1]][cutoff_ind])
+  # printf(" Prec = %.3f\n", pr_curve@y.values[[1]][cutoff_ind])
+  # printf("  Rec = %.3f\n", pr_curve@x.values[[1]][cutoff_ind])
+  # printf(" Spec = %.3f\n", roc@x.values[[1]][cutoff_ind])
+  
+  if (bac[1] != -1)
+    printf(", BalAcc %.3f", mean(bac))
+  printf("\n")
+  
+  
+  invisible(rocr_pred)
+  
+  if (repar) par(mfrow=c(1,1))
+}
+
+
+
+######################################
+##
+##  link_prediction.r
+##
+######################################
 
 
 score = function(g, type="-dist", sort = FALSE, scale=TRUE){
@@ -179,122 +311,6 @@ delete_edges_rand = function(g, p=0.3){
   return(g_result)
 }
 
-last = function(x, ...){
-  tail(x, n=1, ...)
-}
-
-linelight <- function(x,y, lty='dashed', col='lightgray', ...) {
-  # highlight a point with lines running to the axes.
-  left = par('usr')[1]
-  bot = par('usr')[3]
-  segments(left,y, x,y, lty=lty, col=col, ...)
-  segments(x,bot,  x,y, lty=lty, col=col, ...)
-}
-
-
-#https://brenocon.com/blog/2009/04/binary-classification-evaluation-in-r-via-rocr/
-binary_eval <- function(pred,labels, cutoff='naive', repar=TRUE, ...) {
-  # Various binary classification evaluation plots and metrics
-  library(ROCR)
-  # plot(performance(prediction(pred,y),'acc'))
-  rocr_pred = prediction(pred,labels)
-  acc = performance(rocr_pred,'acc')
-  f1 = performance(rocr_pred,'f')
-  auc = performance(rocr_pred,'auc')@y.values[[1]]
-  roc = performance(rocr_pred,'rec','spec')
-  bac = if (rocr_pred@n.pos[[1]] != rocr_pred@n.neg[[1]])
-    sapply(1:length(roc@x.values[[1]]), function(i)
-      mean(c(roc@x.values[[1]][i], roc@y.values[[1]][i])))
-  else
-    rep(-1,length(pred))
-  # sensspec = performance(rocr_pred,'rec','spec')
-  pr_curve = performance(rocr_pred,'prec','rec')
-  rp_curve = performance(rocr_pred,'rec','prec')
-  
-  printf("AUC = %.3f\n", auc)
-  
-  if (cutoff=='naive') {
-    if (all(pred>=0) & all(pred<=1)) {
-      printf("Predictions seem to be probabilities, so ")
-      cutoff = 0.5
-    } else if (any(pred<0) & any(pred>0)) {
-      printf("Predictions seem to be real-valued scores, so ")
-      cutoff = 0
-    } else {
-      warning("cant tell what naive cutoff should be")
-      cutoff = NULL
-    }
-    printf("using naive cutoff %s:\n", cutoff)
-  } else if (class(cutoff)=='character') {
-    printf("Using %s-best cutoff ", cutoff)
-    if (cutoff=='bac') {
-      perf = NULL
-      perf_y = bac
-    } else {
-      perf = performance(rocr_pred, cutoff, ...)
-      perf_y = perf@y.values[[1]]
-    }
-    cutoff_ind = which.max(perf_y)
-    cutoff = if (cutoff=='prbe') perf@x.values[[1]][1] else rocr_pred@cutoffs[[1]][cutoff_ind]
-    printf("%f\n", cutoff)
-  } else {
-    printf("For cutoff %s:\n", cutoff)
-  }
-  cutoff_ind = last(which(rocr_pred@cutoffs[[1]] >= cutoff))
-  
-  if (repar) par(mfrow=c(2,2))
-  
-  pp = function(perf)  {
-    if (length(cutoff_ind)>0 && is.finite(cutoff_ind)) {
-      x=perf@x.values[[1]][cutoff_ind]
-      y=perf@y.values[[1]][cutoff_ind]
-      points(x,y, col='blue')
-      linelight(x,y, col='lightblue')
-    }
-  }
-  plot(acc); pp(acc)
-  plot(f1); pp(f1)
-  plot(roc); pp(roc)
-  abline(a=1,b=-1,lty='dashed',col='gray')
-  legend('bottomleft',legend=sprintf("AUC = %.3f",auc))
-  plot(rp_curve); pp(rp_curve)
-  pp = function(ind,...) points(rp_curve@x.values[[1]][ind], rp_curve@y.values[[1]][ind], ...)
-  best_f1 = which.max(f1@y.values[[1]])
-  pp(best_f1, pch=2,col='green')
-  f05 = performance(rocr_pred,'f',beta=0.5)
-  best_f05 = which.max(f05@y.values[[1]])
-  pp(best_f05,pch=2,col='green')
-  f2 = performance(rocr_pred,'f',beta=2)
-  best_f2 = which.max(f2@y.values[[1]])
-  pp(best_f2,pch=2,col='green')
-  
-  prbe = performance(rocr_pred,'prbe')@y.values[[1]]
-  linelight(prbe,prbe,col='lightgray')
-  
-  # printf("Acc = %.3f\n", mean((pred >= cutoff) == (labels > 0)))
-  printf("Acc %.3f, ", acc@y.values[[1]][cutoff_ind])
-  
-  printf("  F %.3f, Prec %.3f, Rec %.3f, Spec %.3f",
-         f1@y.values[[1]][cutoff_ind],
-         pr_curve@y.values[[1]][cutoff_ind],
-         pr_curve@x.values[[1]][cutoff_ind],
-         roc@x.values[[1]][cutoff_ind])
-  # printf(" Prec = %.3f\n", pr_curve@y.values[[1]][cutoff_ind])
-  # printf("  Rec = %.3f\n", pr_curve@x.values[[1]][cutoff_ind])
-  # printf(" Spec = %.3f\n", roc@x.values[[1]][cutoff_ind])
-  
-  if (bac[1] != -1)
-    printf(", BalAcc %.3f", mean(bac))
-  printf("\n")
-  
-  
-  invisible(rocr_pred)
-  
-  if (repar) par(mfrow=c(1,1))
-}
-
-
-
 evaluate_predictions = function(g, g_obs, types=c("1/dist", 
                                                "common neighbors", 
                                                "normalized common neighbors",
@@ -357,6 +373,8 @@ evaluate_predictions = function(g, g_obs, types=c("1/dist",
   return(list(result=result, metrics=metrics))
 }
 
+
+
 summary_predictions = function(g,
                                n_sample=10, 
                                p_v=seq(from=0.01,to=0.5,by=0.05), 
@@ -409,6 +427,146 @@ summary_predictions = function(g,
 
   return(list(results=results_long, results_summary=results_summary_long))
 }
+
+
+
+
+
+
+######################################
+##
+## vertexattr_prediction.r
+## 
+######################################
+
+
+nnm_node = function(g, attr="label", node, fun="freq"){
+  result = NA
+  if (fun=="mean"){
+    result = mean(vertex_attr(g,attr, index=unlist(neighborhood(g, order=1, nodes=node, mindist=1))), na.rm=TRUE)
+  } else if (fun=="freq"){
+    tt = table(vertex_attr(g,attr, index=unlist(neighborhood(g, order=1, nodes=node, mindist=1))), useNA="no")
+    result = as(names(tt[which.max(tt)]), class(vertex_attr(g,attr)))
+  } 
+  return(result)
+}
+# vertex_attr(g,attr, index=unlist(neighborhood(g, order=1, nodes=node, mindist=1)))
+# [1] 4 4 4 4 6
+# nnm_node(g,attr, 1, "mean")
+# [1] 4.4
+# > nnm_node(g,attr, 1, "freq")
+# [1] 4
+
+nnm = function(g, attr="label", fun="freq"){
+  attr_result = unlist(sapply(V(g), function(x) nnm_node(g, attr, x, fun)))
+  return(attr_result)
+}
+
+
+delete_vertex_attr_rand = function(g, attr="label", p=0.3){
+  g_result = g
+  vertex_attr_to_delete = ifelse(runif(vcount(g_result))<p,TRUE,FALSE)
+  vertex_attr(g_result, attr)[vertex_attr_to_delete] = NA
+  return(g_result)
+}
+
+
+evaluate_predictions_vertexattr = function(g, g_obs, attr, fun="mean", metric="acc"){
+  
+  require(ROCR)
+  
+  vertex_attr_deleted = is.na(vertex_attr(g_obs, attr)) #vector booleando con los atributos borrados
+  vertex_attr_actual = vertex_attr(g, attr)[vertex_attr_deleted] #vector con los atributos reales
+  vertex_attr_actual = factor(vertex_attr_actual, levels=unique(vertex_attr(g, attr)))
+  
+  #computo atributos predichos
+  vertex_attr_pred  = nnm(g_obs,attr, fun)[vertex_attr_deleted] #vector con los atributos predichos
+  #
+  result=data.frame(true = vertex_attr_actual, pred=vertex_attr_pred)
+  metrics = numeric()
+  if (metric=="auc"){
+    pred <- prediction(vertex_attr_pred, vertex_attr_actual)
+    metrics = tryCatch({
+      performance(pred, "auc")@y.values[[1]]
+    }, error = function(e) {
+      NaN
+    })
+  } else if (metric=="f"){
+    pred <- prediction(vertex_attr_pred, vertex_attr_actual)
+    cutoff_ind = tail(which(pred@cutoffs[[1]] >= 0.5), n=1)
+    perf = performance(pred,'f')
+    metrics = perf@y.values[[1]][cutoff_ind]
+  } else if (metric=="acc"){
+    
+    if (length(unique(vertex_attr(g, attr)))==2){ #binario
+      vertex_attr_pred = ifelse(vertex_attr_pred >= 0.5, 1,0)
+    } else { #varias clases
+      vertex_attr_pred = factor(vertex_attr_pred, levels=unique(vertex_attr(g, attr)))  
+    }
+    #
+    # computo la precision
+    cm = as.matrix(table(pred=vertex_attr_pred, actual=vertex_attr_actual)) # create the confusion matrix
+    metrics = sum(diag(cm)) / sum(cm)
+  }
+  return(list(result=result, metrics=metrics))
+}
+
+
+
+summary_predictions_vertexattr = function(g,
+                                          attr,
+                                         n_sample=10, 
+                                         p_v=seq(from=0.01,to=0.5,by=0.05), 
+                                         fun="mean",
+                                         metric="acc",
+                                         cores=1){
+  
+  require(reshape2)
+  
+  if (cores==1){
+    results = data.frame()
+    for (p in p_v){
+      for (n in 1:n_sample){
+        printf("%s %s %s",Sys.time(), p, n)
+        g_obs = delete_vertex_attr_rand(g,attr, p=0.2)
+        r = evaluate_predictions_vertexattr(g, g_obs, attr, fun, metric)
+        r$metrics["p"] = p
+        r$metrics["n"] = n
+        results = rbind(results, r$metrics)
+      }
+    }
+    names(results) = c(metric,"p","n")
+    
+  }else{
+    require(foreach)
+    require(doMC)
+    registerDoMC(cores=cores) 
+    results = foreach(n = 1:n_sample, .combine=rbind) %dopar% {
+      results_in = data.frame()
+      for (p in p_v){
+        printf("%s %s %s",Sys.time(), p, n)
+        g_obs = delete_vertex_attr_rand(g,attr, p=0.2)
+        r = evaluate_predictions_vertexattr(g, g_obs, attr, fun, metric)
+        r$metrics["p"] = p
+        r$metrics["n"] = n
+        results_in = rbind(results_in, r$metrics)
+      }
+      names(results_in) = c(metric,"p","n")
+      return(results_in)
+    }
+  }
+  
+  results_summary = aggregate(. ~ p, results[,setdiff(names(results),"n")], FUN="mean")
+  results_summary_long = melt(results_summary, id.vars=c("p"))
+  names(results_summary_long) = c("p","metric",metric)
+
+  results_long = melt(results, id.vars=c("p","n"))
+  names(results_long) = c("p","n","metric",metric)
+
+  return(list(results=results_long, results_summary=results_summary_long))
+}
+
+
 
 
 
